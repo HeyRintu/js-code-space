8f313e007ae1234e7686aa711bed6e80d7a63690
Rintu Mondal <rintumondal1998@gmail.com>
2023-09-23 12:14:13 +0000

    new changes

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -2,3 +2,59 @@
 
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+console.log(myArray);
+
+const newArry = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+console.log(newArry);
+
+// +++++++++++++++++++++++++ array methods +++++++++++++++++++++++++++++
+
+
+// toString() method  
+
+console.log(myArray.toString());
+
+// length method  
+
+console.log(myArray.length);
+
+// join method
+
+console.log(myArray.join(' '));
+
+// pop method -- modify the original array
+
+let mypopArray = myArray
+console.log(mypopArray.pop());
+console.log(mypopArray)
+
+// push method -- modify the original array
+
+let mypushArray = myArray
+console.log(mypushArray.push(10));
+console.log(mypushArray)
+
+// shift method -- modify the original array
+
+let myshiftArray = myArray
+console.log(myshiftArray.shift());
+console.log(myshiftArray)
+
+// unshift method -- modify the original array
+
+let myunshiftArray = myArray
+console.log(myunshiftArray.unshift(0));
+console.log(myunshiftArray)
+
+// delete method -- modify the original array
+
+let mydeleteArray = myArray
+console.log(delete mydeleteArray[0]);
+console.log(mydeleteArray)
+
+// splice method -- modify the original array
+
+let myspliceArray = myArray
+console.log(myspliceArray.splice(0, 1));
+console.log(myspliceArray)
---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// if
+
+const isUserloggedIn = true
+
+if(isUserloggedIn) {
+    console.log(`User is logged In is ${isUserloggedIn}`);
+}
+
+// <, >, <=, >=, ==, ===, !=, !==,
+
+
+let score = 5400;
+
+if (score > 5000) {
+
+    const power = "fly"
+    console.log(`The power is ${power}`);
+    
+}
+else {
+    console.log(`You have no power`);
+}
+
+
+// const bal = 5000;
+// if(bal >= 1000)  new_bal = 43, console.log("test", new_bal);
+
+// if(bal >= 5000) console.log("perfect test", new_bal)
