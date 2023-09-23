f3bb3a3ad03a402ff4786734c36b2f02bfa444a2
Rintu Mondal <rintumondal1998@gmail.com>
2023-09-23 10:18:43 +0000

    new file added

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,16 +1,122 @@
-let myName = "Rintu";
-let age = 24;
+// let myName = "Rintu";
+// let age = 24;
 
-console.log(`My name is ${myName} and I am ${age} years old.`);
+// console.log(`My name is ${myName} and I am ${age} years old.`);
 
 
-let mystr = new String("Rintu");
+// let mystr = new String("Rintu");
 
-console.log(mystr);
+// console.log(mystr);
 
-//String methods
+// //String methods
 
-let str1 = mystr.charAt(0);
-console.log(str1);
+// let str1 = mystr.charAt(0);
+// console.log(str1);
 
-let str2 = mystr.endsWith("u");
+// let str2 = mystr.endsWith("u");
+
+// CHARAT(X)
+
+    let mystr2 = "Rintu";
+    console.log(mystr2.charAt(0));
+
+// CONCAT(X)
+
+    let mystr3 = "Rintu";
+    let mystr4 = " Mondal";
+
+    console.log(mystr3.concat(mystr4));
+
+// INCLUDES(X)
+
+    let mystr5 = "Rintu";
+    console.log(mystr5.includes("R"));
+
+// ENDSWITH(X)
+
+    let mystr6 = "Rintu";
+    console.log(mystr6.endsWith("u"));
+
+// INDEXOF(X)
+
+    let mystr7 = "Rintu";
+    console.log(mystr7.indexOf("t"));
+
+// LASTINDEXOF(X)
+
+    let mystr8 = "Rintu";
+    console.log(mystr8.lastIndexOf("t"));
+
+// LENGTH(X)
+
+    let mystr9 = "Rintu";
+    console.log(mystr9.length);
+
+// MATCH(X)
+
+    let mystr10 = "Rintu";
+    console.log(mystr10.match("R"));
+
+// REPEAT(X)
+
+    let mystr11 = "Rintu ";
+    console.log(mystr11.repeat(5));
+
+// REPLACE(X)
+
+    let mystr12 = "Rintu";
+    console.log(mystr12.replace("Rintu", "Rintu Mondal"));
+
+// SEARCH(X)   
+
+    let mystr13 = "Rintu";
+    console.log(mystr13.search("R"));
+
+// SLICE(X)
+
+    let mystr14 = "Rintu";
+    console.log(mystr14.slice(0, 3));
+
+// SPLIT(X)
+
+    let mystr15 = "Rintu";
+    console.log(mystr15.split(""));
+
+// STARTSWITH(X)
+
+    let mystr16 = "Rintu";
+    console.log(mystr16.startsWith("R"));
+
+// SUBSTRING(X)
+
+    let mystr17 = "Rintu";
+    console.log(mystr17.substring(0, 3));
+
+
+// TOLOWERCASE(X)
+
+    let mystr18 = "Rintu";
+    console.log(mystr18.toLowerCase());
+
+// TOUPPERCASE(X)
+
+    let mystr19 = "Rintu";
+    console.log(mystr19.toUpperCase());
+
+// TRIM(X)
+
+    let mystr20 = "        Rintu         ";
+    console.log(mystr20.trim());
+
+// TRIMEND(X)
+
+    let mystr21 = "Rintu             ";
+    console.log(mystr21.trimEnd());
+
+// TRIMSTART(X)
+
+    let mystr22 = "      Rintu       ";
+    console.log(mystr22.trimStart());
+
+
+    
--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,4 +1,35 @@
 const score = 10000;
+console.log(score);
 
 const myNum = new Number(100);
 console.log(myNum);
+
+console.log(typeof myNum);
+console.log(typeof score);
+
+console.log(myNum.toString().length);
+
+console.log(myNum.toFixed(2));
+
+
+//+++++++++++++++++++++++++++++++++++++++ Math Object ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.round(2.4));
+console.log(Math.abs(-2.4));
+console.log(Math.ceil(2.4));
+console.log(Math.floor(2.4));
+console.log(Math.sqrt(2.4));
+console.log(Math.pow(2, 4));
+console.log(Math.min(2, 4, 5, 6, 7, 8, 9));
+console.log(Math.max(2, 4, 5, 6, 7, 8, 9));
+
+console.log(Math.random());
+
+
+const min = 15;
+const max = 50;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
