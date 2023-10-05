7dfd7ac2b0516082632c4643e33f69a39687e4d0
Rintu Mondal <rintumondal1998@gmail.com>
2023-10-05 14:45:55 +0000

    new file added

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,18 @@
+//********truthy and falsy******** */
+
+const isUserloggedIn = []
+
+if (isUserloggedIn) {
+    console.log(`User is logged In `);
+}
+    else{
+        console.log(`User is not logged In`);  
+}
+
+//falsey values
+
+// false, 0, -0, "", null, undefined, NaN
+
+//truthy values
+
+// true, 1, -1, "0", "false", [], {}, function(){}
